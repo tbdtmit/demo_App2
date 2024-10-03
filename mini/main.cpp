282c7491fbdfd9ecbced2002491d8e6b7050c73a
tbdtmit <167021609+tbdtmit@users.noreply.github.com>
2024-10-03 23:18:26 +0700

    Add project files.

---
+++ mini/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <QApplication>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QMainWindow>
+#include <QMessageBox>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    QApplication a(argc, argv);
+
+    // Tạo một cửa sổ chính
+    QMainWindow* mainWindow = new QMainWindow();
+    QWidget* centralWidget = new QWidget(mainWindow);
+    mainWindow->setCentralWidget(centralWidget);
+    QVBoxLayout* layout = new QVBoxLayout(centralWidget);
+
+    // Thêm một nút vào cửa sổ chính
+    QPushButton* button = new QPushButton("Click Me!", centralWidget);
+    layout->addWidget(button);
+
+    // Khi nút được nhấn, hiển thị một thông báo
+    QObject::connect(button, &QPushButton::clicked, [&]() {
+        std::cout << "Vl" << std::endl;
+         QMessageBox::information(mainWindow, "Message", "Button clicked!");
+        });
+
+    mainWindow->show();
+    return a.exec();
+}
---
+++ mini/qt_static_plugins.cpp
@@ -0,0 +1,7 @@
+#include <QtCore/QtPlugin>
+#ifdef Q_OS_WIN
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+#endif // !DESKTOP_APP_DISABLE_DBUS_INTEGRATION
+
+
+
